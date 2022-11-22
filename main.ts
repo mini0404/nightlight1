d5a9a9860f2c01818cf381acaa0f136e5fbf44c9
mini0404 <118795631+mini0404@users.noreply.github.com>
2022-11-22 16:40:40 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 basic.forever(function () {
     if (input.lightLevel() < 100) {
         basic.showLeds(`
-            # # # # #
-            # . # . #
-            # # # # #
-            . # # # .
+            . . . . .
+            . # . # .
+            . . . . .
             # . . . #
+            . # # # .
             `)
     } else {
         basic.clearScreen()
